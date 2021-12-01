da51f6605033753743bd0f9464ca431638c61876
Jakub Barczyk <jakubbarczyk@yahoo.com>
2021-12-01 22:05:52 +0100

    Merge mouse events

--- src/index.js
+++ src/index.js
@@ -1,23 +1,77 @@
 import { fromEvent, merge } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { filter, map, startWith, tap, throttleTime } from 'rxjs/operators';
 import { MouseButton } from './mouse-button';
 
 /**
- * Snappy JS
+ * SnappyJS
  */
 const playground = document.querySelector('.playground');
+const playgroundBoundingClientRect = playground.getBoundingClientRect();
+const playgroundOffsetTop = playgroundBoundingClientRect.top;
+const playgroundOffsetLeft = playgroundBoundingClientRect.left;
+
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+context.canvas.height = playgroundBoundingClientRect.height;
+context.canvas.width = playgroundBoundingClientRect.width;
 
 const contextMenu = fromEvent(playground, 'contextmenu').pipe(
     tap(event => event.preventDefault())
 );
 
 const mouseDown = fromEvent(playground, 'mousedown').pipe(
-    tap(event => {
-        console.dir('click', event);
-        console.warn(`Shift key: ${event.shiftKey}`);
-        console.warn(`Left mouse button: ${event.buttons === MouseButton.Primary}`);
-        console.warn(`Right mouse button: ${event.buttons === MouseButton.Secondary}`);
+    filter(({ buttons }) => buttons === MouseButton.Primary),
+    map(event => ({
+        mousedownX: event.clientX - playgroundOffsetLeft,
+        mousedownY: event.clientY - playgroundOffsetTop
+    })),
+    tap(({ mousedownX, mousedownY }) => {
+        context.beginPath();
+        context.moveTo(mousedownX, mousedownY);
     })
 );
 
-merge(contextMenu, mouseDown).subscribe();
+const mouseMove = fromEvent(playground, 'mousemove').pipe(
+    throttleTime(128),
+    map(event => ({
+        mousemoveX: event.clientX - playgroundOffsetLeft,
+        mousemoveY: event.clientY - playgroundOffsetTop
+    }))
+);
+
+const mouseUp = fromEvent(playground, 'mouseup').pipe(
+    map(event => ({
+        mouseupX: event.clientX - playgroundOffsetLeft,
+        mouseupY: event.clientY - playgroundOffsetTop
+    })),
+    tap(({ mouseupX, mouseupY }) => {
+        context.lineTo(mouseupX, mouseupY);
+        context.stroke();
+    })
+);
+
+const mouseMoveWithShift = initialEvent => mouseMove.pipe(
+    startWith(initialEvent),
+    // takeUntil(mouseUp)
+);
+
+const mouseMoveWithoutShift = initialEvent => mouseMove.pipe(
+    startWith(initialEvent),
+    // takeUntil(mouseUp)
+);
+
+// Blocks context menu
+contextMenu.subscribe();
+
+/* ORIGINAL
+    mouseDown.pipe(
+        switchMap(event => iif(
+            () => event.shiftKey,
+            mouseMoveWithShift(event),
+            mouseMoveWithoutShift(event)
+        ))
+    ).subscribe(); 
+ */
+
+merge(mouseDown, mouseUp).subscribe();
