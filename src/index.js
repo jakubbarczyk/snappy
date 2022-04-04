962d25ed50c2161241d92fa4286981eca62498e0
Jakub Barczyk <jakubbarczyk@yahoo.com>
2022-04-04 12:09:51 +0200

    Introduce snapping behavior

--- src/index.js
+++ src/index.js
@@ -1,77 +1,94 @@
-import { fromEvent, merge } from 'rxjs';
-import { filter, map, startWith, tap, throttleTime } from 'rxjs/operators';
-import { MouseButton } from './mouse-button';
+import { animationFrameScheduler, fromEvent } from 'rxjs';
+import { concatMap, filter, map, share, subscribeOn, takeUntil, tap } from 'rxjs/operators';
 
-/**
- * SnappyJS
- */
-const playground = document.querySelector('.playground');
+// PLAYGROUND
+const playground = document.querySelector('#snappy');
 const playgroundBoundingClientRect = playground.getBoundingClientRect();
 const playgroundOffsetTop = playgroundBoundingClientRect.top;
 const playgroundOffsetLeft = playgroundBoundingClientRect.left;
 
-const canvas = document.getElementById('canvas');
+// CANVAS
+const canvas = playground.firstElementChild;
 const context = canvas.getContext('2d');
 
 context.canvas.height = playgroundBoundingClientRect.height;
 context.canvas.width = playgroundBoundingClientRect.width;
 
+const clearContext = () => context.clearRect(0, 0, canvas.width, canvas.height);
+
+// CONTEXT MENU
 const contextMenu = fromEvent(playground, 'contextmenu').pipe(
     tap(event => event.preventDefault())
 );
 
-const mouseDown = fromEvent(playground, 'mousedown').pipe(
-    filter(({ buttons }) => buttons === MouseButton.Primary),
-    map(event => ({
-        mousedownX: event.clientX - playgroundOffsetLeft,
-        mousedownY: event.clientY - playgroundOffsetTop
-    })),
-    tap(({ mousedownX, mousedownY }) => {
+// MOUSE UP
+const toMouseUpPosition = event => ({
+    mouseupX: event.clientX - playgroundOffsetLeft,
+    mouseupY: event.clientY - playgroundOffsetTop
+});
+
+const mouseUp = fromEvent(playground, 'mouseup', { passive: true }).pipe(
+    subscribeOn(animationFrameScheduler),
+    map(toMouseUpPosition),
+    share()
+);
+
+// MOUSE MOVE
+const toMouseMovePosition = event => ({
+    mousemoveX: event.clientX - playgroundOffsetLeft,
+    mousemoveY: event.clientY - playgroundOffsetTop,
+    shiftdown: event.shiftKey // is Shift depressed?
+});
+
+const mouseMove = fromEvent(playground, 'mousemove', { passive: true }).pipe(
+    subscribeOn(animationFrameScheduler),
+    map(toMouseMovePosition),
+    share()
+);
+
+// MOUSE DOWN
+const toMouseDownPosition = event => ({
+    mousedownX: event.clientX - playgroundOffsetLeft,
+    mousedownY: event.clientY - playgroundOffsetTop
+});
+
+const mouseDown = fromEvent(playground, 'mousedown', { passive: true }).pipe(
+    subscribeOn(animationFrameScheduler),
+    filter(({ buttons }) => buttons === 1),
+    map(toMouseDownPosition),
+    share()
+);
+
+// SNAPPY
+const drawLine = ({ mousedownX, mousedownY }) => mouseMove.pipe(
+    tap(({ mousemoveX, mousemoveY, shiftdown }) => {
+        clearContext();
         context.beginPath();
         context.moveTo(mousedownX, mousedownY);
-    })
-);
 
-const mouseMove = fromEvent(playground, 'mousemove').pipe(
-    throttleTime(128),
-    map(event => ({
-        mousemoveX: event.clientX - playgroundOffsetLeft,
-        mousemoveY: event.clientY - playgroundOffsetTop
-    }))
-);
+        if (shiftdown) {
+            const absX = Math.abs(mousemoveX - mousedownX);
+            const absY = Math.abs(mousemoveY - mousedownY);
+            const sign = Math.sign(mousemoveY - mousedownY) === -1 ? -1 : 1;
 
-const mouseUp = fromEvent(playground, 'mouseup').pipe(
-    map(event => ({
-        mouseupX: event.clientX - playgroundOffsetLeft,
-        mouseupY: event.clientY - playgroundOffsetTop
-    })),
-    tap(({ mouseupX, mouseupY }) => {
-        context.lineTo(mouseupX, mouseupY);
+            if (absY <= absX / 2) context.lineTo(mousemoveX, mousedownY);
+            else if (absY <= absX * 2) context.lineTo(mousemoveX, mousedownY + sign * absX);
+            else context.lineTo(mousedownX, mousemoveY);
+        } else {
+            context.lineTo(mousemoveX, mousemoveY);
+        }
+
         context.stroke();
-    })
+    }),
+    takeUntil(mouseUp)
 );
 
-const mouseMoveWithShift = initialEvent => mouseMove.pipe(
-    startWith(initialEvent),
-    // takeUntil(mouseUp)
+const snappy = mouseDown.pipe(
+    concatMap(drawLine)
 );
 
-const mouseMoveWithoutShift = initialEvent => mouseMove.pipe(
-    startWith(initialEvent),
-    // takeUntil(mouseUp)
-);
-
-// Blocks context menu
+// MAIN
 contextMenu.subscribe();
-
-/* ORIGINAL
-    mouseDown.pipe(
-        switchMap(event => iif(
-            () => event.shiftKey,
-            mouseMoveWithShift(event),
-            mouseMoveWithoutShift(event)
-        ))
-    ).subscribe(); 
- */
-
-merge(mouseDown, mouseUp).subscribe();
+mouseUp.subscribe();
+mouseMove.subscribe();
+snappy.subscribe();
--- src/mouse-button.js
+++
@@ -1,8 +0,0 @@
-export const MouseButton = {
-    None: 0,
-    Primary: 1,
-    Secondary: 2,
-    Auxiliary: 4,
-    BrowserBack: 8,
-    BrowserForward: 16
-};
